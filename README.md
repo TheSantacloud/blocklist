1a9d6cbe0e4420f25935f0fa1b05bd2b2c893836
Dor Munis <5811812+dormunis@users.noreply.github.com>
2024-06-17 09:41:43 +0300

    feat: changed to be extension click based rather than popup

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ Private website blocker
 ## TODO:
 
 - [X] Create a popup to enable/disable
+- [X] Change the operation to be icon based rather than popup
 - [ ] Per URL blocking
 - [ ] Time-based blocking
+- [ ] Connect to a list of articles that I might wanna read instead and suggest them
--- background.js
+++ background.js
@@ -1,4 +1,24 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.local.set({ blockListEnabled: true }, function() { });
+    chrome.storage.local.set({ blockListEnabled: true });
+});
+
+chrome.action.onClicked.addListener(() => {
+    chrome.storage.local.get('blockListEnabled', (data) => {
+        const newState = !data.blockListEnabled;
+        chrome.storage.local.set({ blockListEnabled: newState }, () => {
+            updateIcon(newState);
+        });
+    });
+});
+
+function updateIcon(state) {
+    const iconPath = state ? 'assets/icon_color_128.png' : 'assets/icon_gray_128.png';
+    chrome.action.setIcon({ path: iconPath });
+}
+
+chrome.runtime.onStartup.addListener(() => {
+    chrome.storage.local.get('blockListEnabled', (data) => {
+        updateIcon(data.blockListEnabled);
+    });
 });
 
