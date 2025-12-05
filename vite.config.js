bf6999f0f233f14ec4bccaa30b00c7188c25b025
Dor Munis <5811812+dormunis@users.noreply.github.com>
2025-12-05 18:08:56 +0200

    chore(builds): conventional commits

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,20 @@ import { defineConfig } from 'vite'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './src/manifest.json'
 
-export default defineConfig({
-    root: 'src',
-    build: {
-        outDir: '../dist',
-        emptyOutDir: true,
-    },
-    plugins: [
-        crx({ manifest }),
-    ],
+export default defineConfig(({ mode }) => {
+    const isDev = mode === 'dev'
+    const finalManifest = isDev
+        ? { ...manifest, name: `${manifest.name} (dev)` }
+        : manifest
+
+    return {
+        root: 'src',
+        build: {
+            outDir: '../dist',
+            emptyOutDir: true,
+        },
+        plugins: [
+            crx({ manifest: finalManifest }),
+        ],
+    }
 })
